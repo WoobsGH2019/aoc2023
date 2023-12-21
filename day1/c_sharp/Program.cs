b254f96918da5440e12f3663002360b647253ea1
Robert <RobertGithub@wubortsoft.com>
2023-12-20 21:45:36 -0500

    AOC files for day 1, 2, & 3

---
+++ day1/c_sharp/Program.cs
@@ -0,0 +1,266 @@
+﻿using System.Data;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// https://adventofcode.com/2023/day/1
+
+bool isNumber = false;
+int number, lineNumber = 0, sum;
+int[] digitCollection = new int[1000];
+string[] sToNumPatterns = { "one\\z", "two\\z", "three\\z", "four\\z", "five\\z", "six\\z", "seven\\z", "eight\\z", "nine\\z" };
+Match patternMatch;
+string firstDigit = System.String.Empty;
+string secondDigit = System.String.Empty;
+string? line = System.String.Empty;
+string lex = "";
+
+
+#region part2
+try
+{
+    using (StreamReader reader = new StreamReader(@"/home/user/repos/aoc2023/day1/c_sharp/input/part2Data"))
+    {
+        while(line != null)
+        {
+            line = reader.ReadLine();
+            if(!System.String.IsNullOrWhiteSpace(line)) {
+                for(int pos=0; pos<line.Length; pos++) 
+                {
+                    System.ReadOnlySpan<char> character = line[pos].ToString();
+                    isNumber = Int32.TryParse(character, out number);
+
+                    if(isNumber && System.String.IsNullOrWhiteSpace(firstDigit)) {
+                        firstDigit = number.ToString();
+                        lex = "";
+                    }
+                    else if (isNumber && !System.String.IsNullOrWhiteSpace(firstDigit)) {
+                        secondDigit = number.ToString();
+                        lex = "";
+                    }
+                    else {
+                        lex += line[pos];
+
+                        foreach(string stringToMatch in sToNumPatterns)
+                        {
+                            patternMatch = Regex.Match(lex, stringToMatch);
+                            if(patternMatch.Success)
+                            {
+                                // Console.WriteLine($"Match found {stringToMatch} in {lineNumber} with result {patternMatch.Value}.");
+                                switch (patternMatch.Value)
+                                {
+                                    case "one":
+                                        // Console.WriteLine("Detected one");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "1";
+                                        }
+                                        else {
+                                            secondDigit = "1";
+                                        }
+
+                                        break;
+                                    case "two":
+                                        // Console.WriteLine("Detected two");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "2";
+                                        }
+                                        else {
+                                            secondDigit = "2";
+                                        }
+
+                                        break;
+                                    case "three":
+                                        // Console.WriteLine("Detected three");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "3";
+                                        }
+                                        else {
+                                            secondDigit = "3";
+                                        }
+
+                                        break;
+                                    case "four":
+                                        // Console.WriteLine("Detected four");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "4";
+                                        }
+                                        else {
+                                            secondDigit = "4";
+                                        }
+
+                                        break;
+                                    case "five":
+                                        // Console.WriteLine("Detected five");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "5";
+                                        }
+                                        else {
+                                            secondDigit = "5";
+                                        }
+
+                                        break;
+                                    case "six":
+                                        // Console.WriteLine("Detected six");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "6";
+                                        }
+                                        else {
+                                            secondDigit = "6";
+                                        }
+
+                                        break;
+                                    case "seven":
+                                        // Console.WriteLine("Detected seven");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "7";
+                                        }
+                                        else {
+                                            secondDigit = "7";
+                                        }
+
+                                        break;
+                                    case "eight":
+                                        // Console.WriteLine("Detected eight");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "8";
+                                        }
+                                        else {
+                                            secondDigit = "8";
+                                        }
+
+                                        break;
+                                    case "nine":
+                                        // Console.WriteLine("Detected nine");
+                                        if(System.String.IsNullOrWhiteSpace(firstDigit)) {
+                                            firstDigit = "9";
+                                        }
+                                        else {
+                                            secondDigit = "9";
+                                        }
+
+                                        break;
+                                    default:
+                                        // Console.WriteLine("No matches found.  Resume parsing");
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    // Console.WriteLine($"LineNumber is {lineNumber}");
+                }
+
+                //Reached end of line
+                if(System.String.IsNullOrWhiteSpace(secondDigit)) {
+                    secondDigit = firstDigit;
+                }
+
+                digitCollection[lineNumber] = Int32.Parse($"{firstDigit}{secondDigit}");
+                // Console.WriteLine($"digitCollection[{lineNumber}] has a value of {digitCollection[lineNumber]}.");
+
+                // Zero everything out before next ReadLine.
+                firstDigit = System.String.Empty;
+                secondDigit = System.String.Empty;
+                lex = "";
+
+                // Increment index
+                lineNumber++;
+            }
+            else
+            {
+                Console.WriteLine("Reached EOF!");
+            }
+        }
+    }
+
+    // Console.WriteLine("Line numbers are as follows.");
+    sum = 0;
+    sum = digitCollection.Sum();
+
+    // Console.WriteLine($"digitCollection has a length of {digitCollection.Length}");
+    Console.WriteLine($"Part 2 sum: {sum}.");
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Broad exception caught.  Make sure file exists and review variable assigments.");
+    throw;
+}
+#endregion
+
+
+
+
+#region part1
+
+// bool sameLine, isDigit, isSingleDigitLine;
+// int number, lineNumber = 0, sum = 0;
+// int[] digitCollection = new int[1000];
+// string digitBuff = "";
+
+// try
+// {
+//     isSingleDigitLine = false;
+//     sameLine = false;
+
+//     foreach(string line in File.ReadLines(@"/home/robert/repos/aoc2023/day1/c_sharp/input/sampleData"))
+//     {
+//         for(int pos=0; pos < line.Length; pos++)
+//         {
+//             ReadOnlySpan<char> character = line[pos].ToString();
+            
+//             isDigit = int.TryParse(character, out number);
+
+//             if(isDigit && sameLine == true) {
+//                 // Console.WriteLine("First condition success!");
+//                 digitBuff += number; //line[pos];
+//                 isSingleDigitLine = false;
+//             }
+//             else if(isDigit && sameLine == false) {
+//                 // Console.WriteLine("Second condition success!");
+//                 digitBuff += number; //line[pos];
+//                 sameLine = true;
+//                 isSingleDigitLine = true;
+//             }
+
+//             if(pos == ((line.Length) - 1)) {
+                
+//                 if(isSingleDigitLine) {
+//                     digitBuff += digitBuff;
+//                     // Console.WriteLine("The value of single digitBuff is {0}", digitBuff);
+//                 }
+//                 else {
+//                     if(digitBuff.Length > 2)
+//                     {
+//                         string tempBuf = "";
+//                         tempBuf += digitBuff[0].ToString() + digitBuff[digitBuff.Length-1].ToString();
+//                         digitBuff = "";
+//                         digitBuff += tempBuf;
+//                     }
+//                     // Console.WriteLine("The value of digitBuff is {0}", digitBuff);
+//                 }
+
+//                 digitCollection[lineNumber] = Int32.Parse(digitBuff);
+//                 // Console.WriteLine("digitCollection contains the following {0}", digitCollection[lineNumber]);
+                
+//                 lineNumber++;
+//                 digitBuff = "";
+//                 sameLine = false;
+//             }
+//         }
+//     }
+
+//     // Console.WriteLine("Line numbers are as follows.");
+//     for(int index=0; index < digitCollection.Length; index++) {
+//         sum += digitCollection[index];
+//         // Console.WriteLine(digitCollection[index]);
+//     }
+
+//     Console.WriteLine($"The sum has a value of {sum}.");
+// }
+// catch (System.Exception)
+// {
+//     Console.WriteLine("Broad exception caught.  Make sure file exists and review variable assigments.");
+//     throw;
+// }
+#endregion
+    
---
+++ day2/c_sharp/Program.cs
@@ -0,0 +1,128 @@
+﻿// See https://aka.ms/new-console-template for more information
+// AOC: https://adventofcode.com/2023/day/2
+// AOC Part deux: https://adventofcode.com/2023/day/2#part2
+// Input: https://adventofcode.com/2023/day/2/input
+
+using System.IO;
+using System.Collections.Immutable;
+using System.Drawing;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata;
+
+namespace AOC
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            // string path = @"./input/part1Sample";
+            string path = @"./input/part1Data";
+            
+            string[] data;
+
+            // Pattern matching against the elf draws.  Retrieves the number of cubes of each color.
+            const string RED = "\\d* red", GREEN = "\\d* green", BLUE = "\\d* blue";
+
+            // Part 1
+            const int REDLIMIT = 12, GREENLIMIT = 13, BLUELIMIT = 14;
+            // Part 2
+            int maxRed, maxGreen, maxBlue;
+
+            int gameNumber = 0, redCubesDrawn = 0, greenCubesDrawn = 0, blueCubesDrawn = 0;
+            string gameInfo = "", cubeDraws = "";
+            int drawCount, startIndex = 0, sumOfGames = 0, sumOfPowerOfDraws = 0;
+            string[] draw;
+            bool goodGame;
+            Match patternMatch;
+
+            if(!System.IO.File.Exists(path)) {
+                Console.WriteLine($"File does not exist at {path}.");
+                System.Environment.Exit(1);
+            }
+
+            try
+            {
+                data = File.ReadAllLines(path, System.Text.Encoding.UTF8);
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+
+            // Logic & Impl
+
+            foreach(string line in data)
+            {
+                gameInfo = line.Split(':')[0];
+                cubeDraws = line.Split(':')[1];
+
+                goodGame = true;
+                maxRed = maxGreen = maxBlue = 0;
+                startIndex = gameInfo.LastIndexOf(' ');
+                gameNumber = int.Parse(gameInfo.Substring(startIndex, (gameInfo.Length-startIndex)));
+
+                drawCount = 0;
+                foreach(char character in cubeDraws)
+                {
+                    if(character == ';') {
+                        drawCount++;
+                    }
+                }
+
+                draw = cubeDraws.Split(';');
+                for(int count = 0; count <= drawCount; count++)
+                {
+                    patternMatch = Regex.Match(draw[count], RED);
+                    if(patternMatch.Success) {
+                        redCubesDrawn = int.Parse((patternMatch.Value).Split(' ')[0]);
+
+                        if(redCubesDrawn > REDLIMIT) {
+                            goodGame = false;
+                        }
+
+                        if(redCubesDrawn > maxRed) {
+                            maxRed = redCubesDrawn;
+                        }
+                    }
+
+                    patternMatch = Regex.Match(draw[count], GREEN);
+                    if(patternMatch.Success) {
+                        greenCubesDrawn = int.Parse((patternMatch.Value).Split(' ')[0]);
+
+                        if(greenCubesDrawn > GREENLIMIT) {
+                            goodGame = false;
+                        }
+
+                        if(greenCubesDrawn > maxGreen) {
+                            maxGreen = greenCubesDrawn;
+                        }
+                    }
+
+                    patternMatch = Regex.Match(draw[count], BLUE);
+                    if(patternMatch.Success) {
+                        blueCubesDrawn = int.Parse((patternMatch.Value).Split(' ')[0]);
+
+                        if(blueCubesDrawn > BLUELIMIT) {
+                            goodGame = false;
+                        }
+
+                        if(blueCubesDrawn > maxBlue) {
+                            maxBlue = blueCubesDrawn;
+                        }
+                    }
+                }
+
+                if(goodGame) {
+                    sumOfGames += gameNumber;
+                }
+
+                sumOfPowerOfDraws += maxRed * maxGreen * maxBlue;
+            }
+
+            Console.WriteLine($"Sum of games is {sumOfGames}");
+            Console.WriteLine($"Sum of power of draws is {sumOfPowerOfDraws}");
+        }
+    }
+}
