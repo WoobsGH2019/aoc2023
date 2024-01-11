2daafed36e1b3389e476ca7ef99ddb3176e2927a
Robert <RobertGithub@wubortsoft.com>
2024-01-10 20:32:49 -0500

    Remove input files and cleanup

---
+++ day5/c_sharp/DataMap.cs
@@ -0,0 +1,66 @@
+namespace AOC
+{
+  class DataMap
+  {
+    private Int64 destination { get; set; }
+    private Int64 source { get; set; }
+    private Int64 range { get; set; }
+
+    public DataMap()
+    {
+      destination = 0;
+      source = 0;
+      range = 0;
+    }
+
+    // Create sort function so we can efficently look where to send previous lookups?
+
+    public void SaveData(string argData)
+    {
+      Int64 destinationData, sourceData, rangeData;
+
+      destinationData = Int64.Parse(argData.Split(' ')[0]);
+      sourceData = Int64.Parse(argData.Split(' ')[1]);
+      rangeData = Int64.Parse(argData.Split(' ')[2]);
+
+      destination = destinationData;
+      source = sourceData;
+      range = rangeData;
+    }
+
+    // This prevents us from looking at initialized empty elements.... gross.
+    public bool CheckRange()
+    {
+      if(range > 0) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    public void PrintData()
+    {
+      Console.WriteLine($"Destination: {destination}; Source: {source}; Range: {range}");
+    }
+
+    public Int64 CheckSource(Int64 argValueToCheck)
+    {
+      Int64 result;
+      Int64 ceiling = source + range;
+
+      if(argValueToCheck >= source && argValueToCheck <= ceiling)
+      {
+        result = (destination - source) + argValueToCheck;
+        //Console.WriteLine($"Result: {result}.");
+        return result;
+      }
+      else
+      {
+        return -1;
+      }
+    }
+
+
+  }
+}
--- day5/c_sharp/Program.cs
+++ day5/c_sharp/Program.cs
@@ -1,25 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-using System;
-using System.IO;
-
 namespace AOC
 {
   class Program
   {
     static void Main(string[] args)
     {
+      Int64 tempDestination;
+      Int64 minSoilDestination, minFertilizerDestination, minWaterDestination, minLightDestination, minTemperatureDestination, minHumidityDestination, minLocationDestination;
+      int blockCount = 0, blockEntryCount = 0;
       string path, seedValues;
       string? line = String.Empty;
-      List<int> seedList = new List<int>();
+      List<Int64> seedList = new List<Int64>();
+      List<Int64> locationList = new List<Int64>();
+
+      // For future me.... this is disgusting.  Maybe linked list in the class to dynamically handle the various elements
+      DataMap[] soilMap = new DataMap[23].Select(sm => new DataMap()).ToArray();
+      DataMap[] fertilizerMap = new DataMap[40].Select(fm => new DataMap()).ToArray();
+      DataMap[] waterMap = new DataMap[14].Select(wm => new DataMap()).ToArray();
+      DataMap[] lightMap = new DataMap[46].Select(lm => new DataMap()).ToArray();
+      DataMap[] tempMap = new DataMap[35].Select(tm => new DataMap()).ToArray();
+      DataMap[] humidityMap = new DataMap[28].Select(hm => new DataMap()).ToArray();
+      DataMap[] locationMap = new DataMap[42].Select(lm => new DataMap()).ToArray();
+
+
 
       // File handling
       try
       {
         if (args.Length == 0)
         {
-          path = @"../input/sampleData";
+          path = @"../input/inputData";
         }
         else
         {
@@ -28,7 +40,6 @@ namespace AOC
 
         using (StreamReader reader = new(path))
         {
-          // First line with seed is the only line with the topic and numbers.
           line = reader.ReadLine();
           if(line != null) 
           {
@@ -38,7 +49,7 @@ namespace AOC
               seedValues = seedValues.TrimStart();
               foreach(string seed in seedValues.Split(' '))
               {
-                seedList.Add(Int32.Parse(seed));
+                seedList.Add(Int64.Parse(seed));
               }
             }
           }
@@ -48,9 +59,45 @@ namespace AOC
             line = reader.ReadLine();
             if (!String.IsNullOrWhiteSpace(line))
             {
-              foreach(int seed in seedList)
+              if(line.Contains(":"))
               {
-                Console.WriteLine($"Seed {seed}.");
+                blockCount++;
+                blockEntryCount = 0;
+                continue;
+              }
+
+              switch(blockCount)
+              {
+                case 1:
+                  soilMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 2:
+                  fertilizerMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 3:
+                  waterMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 4:
+                  lightMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 5:
+                  tempMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 6:
+                  humidityMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  break;
+                case 7:
+                  locationMap[blockEntryCount].SaveData(line);
+                  blockEntryCount++;
+                  // Console.WriteLine($"Case 7: With line {line} with blockCount {blockCount} and blockEntryCount {blockEntryCount}.");
+                  break;
+
               }
             }
           }
@@ -60,6 +107,179 @@ namespace AOC
       {
         throw;
       }
+
+      //Now that we have all the data, let's perform the lookups.
+      foreach(Int64 seed in seedList)
+      {
+        Console.Write($"Seed {seed}, ");
+
+        minSoilDestination = -1;
+        foreach(DataMap soil in soilMap)
+        {
+          if(soil.CheckRange()) {
+            tempDestination = soil.CheckSource(seed);
+
+            // Gets us out of minimum soil destination of -1
+            if(tempDestination >= 0 && minSoilDestination == -1) {
+              minSoilDestination = tempDestination;
+            }
+
+            // Need to find the smallest destination
+            if(tempDestination < minSoilDestination && tempDestination != -1) {
+              // minSoilDestination = tempDestination;
+            }
+          }
+        }
+
+        // Set mininimum to self if not within any ranges.
+        if(minSoilDestination == -1) {
+          minSoilDestination = seed;
+        }
+
+        Console.Write($"soil {minSoilDestination}, ");
+
+        minFertilizerDestination = -1;
+        foreach(DataMap fertilizer in fertilizerMap)
+        {
+          if(fertilizer.CheckRange()) {
+            tempDestination = fertilizer.CheckSource(minSoilDestination);
+
+            if(tempDestination >= 0 && minFertilizerDestination == -1) {
+              minFertilizerDestination = tempDestination;
+            }
+
+            if(tempDestination < minFertilizerDestination && tempDestination != -1) {
+              // minFertilizerDestination = tempDestination;
+            }
+          }
+        }
+
+        if(minFertilizerDestination == -1) {
+          minFertilizerDestination = minSoilDestination;
+        }
+        
+        Console.Write($"fertilizer {minFertilizerDestination}, ");
+
+
+        minWaterDestination = -1;
+        foreach(DataMap water in waterMap)
+        {
+          if(water.CheckRange()) {
+            tempDestination = water.CheckSource(minFertilizerDestination);
+
+            if(tempDestination >= 0 && minWaterDestination == -1) {
+              minWaterDestination = tempDestination;
+            }
+
+            if(tempDestination < minWaterDestination && tempDestination != -1) {
+              // minWaterDestination = tempDestination;
+            }
+          }
+        }
+        
+        if(minWaterDestination == -1) {
+          minWaterDestination = minFertilizerDestination;
+        }
+
+        Console.Write($"water {minWaterDestination}, ");
+
+        minLightDestination = -1;
+        foreach(DataMap light in lightMap)
+        {
+          if(light.CheckRange()) {
+            tempDestination = light.CheckSource(minWaterDestination);
+
+            if(tempDestination >= 0 && minLightDestination == -1) {
+              minWaterDestination = tempDestination;
+            }
+
+            if(tempDestination < minLightDestination && tempDestination != -1) {
+              // minWaterDestination = tempDestination;
+            }
+          }
+        }
+
+        if(minLightDestination == -1) {
+          minLightDestination = minWaterDestination;
+        }
+
+        Console.Write($"light {minLightDestination}, ");
+
+        minTemperatureDestination = -1;
+        foreach(DataMap temp in tempMap)
+        {
+          if(temp.CheckRange()) {
+            tempDestination = temp.CheckSource(minLightDestination);
+
+            if(tempDestination >= 0 && minTemperatureDestination == -1) {
+              minTemperatureDestination = tempDestination;
+            }
+
+            if(tempDestination < minTemperatureDestination && tempDestination != -1) {
+              // minTemperatureDestination = tempDestination;
+            }
+          }
+        }
+        
+        if(minTemperatureDestination == -1) {
+          minTemperatureDestination = minLightDestination;
+        }
+
+        Console.Write($"temperature {minTemperatureDestination}, ");
+
+        minHumidityDestination = -1;
+        foreach(DataMap humidity in humidityMap)
+        {
+          if(humidity.CheckRange()) {
+            tempDestination = humidity.CheckSource(minTemperatureDestination);
+
+            if(tempDestination >= 0 && minHumidityDestination == -1) {
+              minHumidityDestination = tempDestination;
+            }
+
+            if(tempDestination < minHumidityDestination && tempDestination != -1) {
+              // minHumidityDestination = tempDestination;
+            }
+          }
+        }
+        
+        if(minHumidityDestination == -1) {
+          minHumidityDestination = minTemperatureDestination;
+        }
+
+        Console.Write($"humidity {minHumidityDestination}, ");
+
+        minLocationDestination = -1;
+        foreach(DataMap location in locationMap)
+        {
+          if(location.CheckRange()) {
+            tempDestination = location.CheckSource(minHumidityDestination);
+
+            if(tempDestination >= 0 && minLocationDestination == -1) {
+              minLocationDestination = tempDestination;
+            }
+
+            if(tempDestination < minLocationDestination && tempDestination != -1) {
+              // minLocationDestination = tempDestination;
+            }
+          }
+        }
+        if(minLocationDestination == -1) {
+          minLocationDestination = minHumidityDestination;
+        }
+
+        Console.WriteLine($"location {minLocationDestination}.");
+
+        locationList.Add(minLocationDestination);
+      }
+
+      Console.WriteLine($"Minimum location: {locationList.Min()}.");
+      locationList.Sort();
+      foreach(Int64 i in locationList)
+      {
+        Console.WriteLine($"{i}");
+      }
+      // Console.WriteLine($"Sorted Locations: {locationList}");
     }
   }
 }
